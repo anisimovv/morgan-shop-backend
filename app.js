6eff2a4384bb1f56bbe6bb348efb6e15499c0990
Anisimovv <anisimovv@Anisimovvs-MacBook-Pro.local>
2020-12-04 14:58:19 +0200

    build settings test

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const serverless = require("serverless-http")
 
+const router = express.Router; 
+
 const app = express();
 
 const shopRoutes = require("./routes/shop");
 
+
+app.use(shopRoutes);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/.netlify/functions/app', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 
-app.use(shopRoutes);
 
 module.exports = app;
 module.exports.handler = serverless(app);
