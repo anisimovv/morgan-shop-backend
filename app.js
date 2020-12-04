10c4b58598bdd0ab73bd42f4b14df7f250b3ba00
Anisimovv <anisimovv@Anisimovvs-MacBook-Pro.local>
2020-12-04 17:47:36 +0200

    deploy fix

--- app.js
+++ app.js
@@ -7,24 +7,13 @@ const serverless = require("serverless-http")
 
 const router = express.Router; 
 
+const shopRoutes = require("./routes/shop");
+
 const app = express();
 
 app.use(cors());
 
-const shopRoutes = require("./routes/shop");
-
-
 app.use(bodyParser.json());
-
-
-app.use('/.netlify/functions/app', router);  // path must route to lambda
 app.use(shopRoutes);
-// app.use('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'index.html'));
-//   console.log(path.join(__dirname, 'index.html'));
-// });
 
-
-module.exports = app;
-
-module.exports.handler = serverless(app);
+app.listen(8000);
--- controllers/shop.js
+++ controllers/shop.js
@@ -5,7 +5,6 @@ const path = require("path");
 exports.getProducts = (req, res) => {
   setTimeout((() => {
     Product.fetchAll((products) => {
-      res.sendFile(path.join(__dirname, "..", 'index.html'));
       res.json(products);
       res.end();
     });
