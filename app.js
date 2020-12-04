f8cfa185cb4b4bce30d760a82ffa42eea1a0311d
Anisimovv <anisimovv@Anisimovvs-MacBook-Pro.local>
2020-12-04 17:53:24 +0200

    deploy fix

--- app.js
+++ app.js
@@ -3,17 +3,14 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const serverless = require("serverless-http")
-
-const router = express.Router; 
 
 const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 app.use(cors());
-
 app.use(bodyParser.json());
+
 app.use(shopRoutes);
 
 app.listen(process.env.PORT);
